5e664641d8dcc4496c180fe786563d39c13931d1
SarthakKamboj <sarthakkamboj22@gmail.com>
2022-11-22 16:46:34 -0600

    adde some testing

--- SpaceInvaders/main.cpp
+++ SpaceInvaders/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "stack_alloc.h"
 #include "pool_alloc.h"
+#include "frame_alloc.h"
 
-#define TEST_STACK_ALLOC 1
+#define TEST_STACK_ALLOC 0
 #define TEST_POOL_ALLOC 0
+#define TEST_SINGLE_FRAME_STACK_ALLOC 0
+#define TEST_DOUBLE_FRAME_STACK_ALLOC 1
 
 int main() {
 
@@ -52,6 +55,32 @@ int main() {
 
 	for (int i = 0; i < pool_size; i++) {
 		int_pool.allocate();
+	}
+
+#elif TEST_SINGLE_FRAME_STACK_ALLOC == 1
+	single_frame_alloc_t single_frame_alloc;
+	for (int i = 0; i < 2; i++) {
+		single_frame_alloc.handle_sof();
+		int* a_ptr = (int*)single_frame_alloc.allocate(sizeof(int));
+		*a_ptr = 5 * i;
+		char* b_ptr = (char*)single_frame_alloc.allocate(sizeof(char));
+		*b_ptr = 15 * i;
+		double* c_ptr = (double*)single_frame_alloc.allocate(sizeof(double));
+		*c_ptr = 25 * i;
+		single_frame_alloc.handle_eof();
+	}
+
+#elif TEST_DOUBLE_FRAME_STACK_ALLOC == 1
+	double_frame_alloc_t double_frame_alloc;
+	for (int i = 0; i < 4; i++) {
+		double_frame_alloc.handle_sof();
+		int* a_ptr = (int*)double_frame_alloc.allocate(sizeof(int));
+		*a_ptr = 5 * (i + 1);
+		char* b_ptr = (char*)double_frame_alloc.allocate(sizeof(char));
+		*b_ptr = 15 * (i + 1);
+		double* c_ptr = (double*)double_frame_alloc.allocate(sizeof(double));
+		*c_ptr = 25 * (i + 1);
+		double_frame_alloc.handle_eof();
 	}
 
 #endif
