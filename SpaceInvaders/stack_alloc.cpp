ef75cc6411fc54c690543e1320b4e8b12f9551c0
SarthakKamboj <sarthakkamboj22@gmail.com>
2022-11-23 11:20:50 -0600

    removed use of std::vector

--- SpaceInvaders/stack_alloc.cpp
+++ SpaceInvaders/stack_alloc.cpp
@@ -12,8 +12,7 @@ stack_alloc_t::~stack_alloc_t() {
 }
 
 char* stack_alloc_t::allocate(int num_bytes) {
-
-	if (stack_frames.size() == 0) {
+	if (num_stack_frames == 0) {
 		throw std::exception("must declare at least 1 stack frame");
 	}
 
@@ -22,74 +21,32 @@ char* stack_alloc_t::allocate(int num_bytes) {
 	if (new_bytes_alloc > STACK_SIZE) {
 		throw std::exception("stack overflow...cannot allocate");
 	}
+
 	char* cur_saved = cur;
 	cur += num_bytes;
-	*cur = num_bytes;
-	cur += sizeof(int);
-
-	stack_frames[stack_frames.size() - 1].num_bytes += num_bytes + sizeof(int);
-
+	num_bytes_of_cur_stack_frame += num_bytes;
 	return cur_saved;
 }
 
-void stack_alloc_t::deallocate() {
-	if (cur == stack) return;
-
-	if (stack_frames.size() == 0) {
-		throw std::exception("must declare at least 1 stack frame");
+void stack_alloc_t::create_stack_frame() {
+	if (num_stack_frames > 0) {
+		*cur = num_bytes_of_cur_stack_frame;
+		cur += sizeof(num_bytes_of_cur_stack_frame);
 	}
-
-	cur -= sizeof(int);
-	char num_bytes = *cur;
-	cur -= num_bytes;
-	stack_frames[stack_frames.size() - 1].num_bytes -= num_bytes + sizeof(int);
+	num_stack_frames += 1;
+	num_bytes_of_cur_stack_frame = 0;
 }
 
+void stack_alloc_t::remove_stack_frame() {
+	cur -= num_bytes_of_cur_stack_frame;
+	num_stack_frames -= 1;
+	if (num_stack_frames > 0) {
+		num_bytes_of_cur_stack_frame = *cur;
+		cur -= sizeof(num_bytes_of_cur_stack_frame);
+	}
+}
 void stack_alloc_t::clear() {
 	cur = stack;
-	stack_frames.clear();
-}
-
-void stack_alloc_t::create_stack_frame(stack_alloc_t::stack_frame_t& user_stack_frame) {
-	stack_alloc_t::internal_stack_frame_t frame;
-	frame.frame_start = cur;
-	frame.stack_alloc = this;
-	frame.num_bytes = 0;
-	stack_frames.emplace_back(frame);
-
-	user_stack_frame.internal_stack_frame = &stack_frames[stack_frames.size() - 1];
-}
-
-stack_alloc_t::stack_frame_t& stack_alloc_t::stack_frame_t::operator=(stack_alloc_t::stack_frame_t&& other) {
-	if (internal_stack_frame != NULL) {
-		internal_stack_frame->deallocate();
-	}
-
-	internal_stack_frame = other.internal_stack_frame;
-	other.internal_stack_frame = NULL;
-
-	return *this;
-}
-
-stack_alloc_t::stack_frame_t::stack_frame_t() {
-	internal_stack_frame = NULL;
-	std::cout << "frame constructed" << std::endl;
-}
-
-stack_alloc_t::stack_frame_t::~stack_frame_t() {
-	if (internal_stack_frame != NULL) {
-		internal_stack_frame->deallocate();
-		std::cout << "frame destructed" << std::endl;
-	}
-}
-
-stack_alloc_t::stack_frame_t::stack_frame_t(stack_alloc_t::stack_frame_t&& other) {
-	internal_stack_frame = other.internal_stack_frame;
-	other.internal_stack_frame = NULL;
-}
-
-void stack_alloc_t::internal_stack_frame_t::deallocate() {
-	stack_alloc->cur = frame_start;
-	std::cout << "upon destruction, this stack frame had " << num_bytes << " bytes allocated" << std::endl;
-	stack_alloc->stack_frames.pop_back();
+	num_stack_frames = 0;
+	num_bytes_of_cur_stack_frame = 0;
 }
--- SpaceInvaders/stack_alloc.h
+++ SpaceInvaders/stack_alloc.h
@@ -7,40 +7,18 @@
 
 class stack_alloc_t {
 public:
-	struct internal_stack_frame_t {
-		char* frame_start;
-		int num_bytes;
-		stack_alloc_t* stack_alloc;
-
-		void deallocate();
-	};
-
-	class stack_frame_t {
-	public:
-		stack_frame_t();
-		~stack_frame_t();
-		stack_frame_t(stack_frame_t&& other);
-		stack_frame_t& operator=(stack_frame_t&& other);
-
-	private:
-		internal_stack_frame_t* internal_stack_frame;
-		friend class stack_alloc_t;
-	};
-
-public:
 	stack_alloc_t();
 	~stack_alloc_t();
 	char* cur;
-	// allocates a specified amount
 	char* allocate(int num_bytes);
-	// deallocates a specific variable
-	void deallocate();
 	void clear();
-	void create_stack_frame(stack_alloc_t::stack_frame_t& stack_frame);
+	void create_stack_frame();
+	void remove_stack_frame();
 
 private:
 	char* stack;
-	std::vector<internal_stack_frame_t> stack_frames;
+	int num_stack_frames = 0;
+	int num_bytes_of_cur_stack_frame = 0;
 };
 
 
